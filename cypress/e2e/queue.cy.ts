952eac796e5df192eaf8a53e3370a8688d1ecd10
Nikita <prey122@gmail.com>
2023-03-31 00:03:42 +0300

    test 0.0.1

--- cypress/e2e/queue.cy.ts
+++ cypress/e2e/queue.cy.ts
@@ -13,11 +13,11 @@ describe("test queue", () => {
   });
 
   it('check queue', () => {
-    const queue = new Queue<string>(4);
+    
 
     cy.get('[data-testid=input]').type('h');
     cy.get('[data-testid=button]').click();
-    queue.enqueue('h');
+    
     cy.get('[data-testid="testCicrle"]').as('circles');
     cy.get('@circles').eq(0).children().eq(1).should(($div) => should($div, /circle_changing__/));
     cy.wait(500)
@@ -25,23 +25,25 @@ describe("test queue", () => {
     cy.get('@circles').eq(0).children().eq(-1).should('have.text', 'tail');
     cy.get('[data-testid=input]').type('e');
     cy.get('[data-testid=button]').click();
-    queue.enqueue('e');
+    
     cy.get('@circles').eq(1).children().eq(1).should(($div) => should($div, /circle_changing__/));
     cy.wait(500);
     cy.get('@circles').eq(1).children().eq(-1).should('have.text', 'tail');
 
     cy.get('[data-testid=buttonDell]').click();
     cy.get('@circles').eq(0).children().eq(1).should(($div) => should($div, /circle_changing__/));
-    queue.dequeue();
+    
     cy.wait(500);
     cy.get('@circles').eq(0).children().eq(1).should(($div) => should($div, /circle_default__/));
     cy.get('@circles').eq(0).children().eq(0).should('have.text', '');
     cy.get('@circles').eq(1).children().eq(0).should('have.text', 'head');
 
     cy.get('[data-testid=buttonClear]').click();
-    queue.clear();
-
-    expect(queue.isEmpty()).to.be.true
+    
+    cy.get('@circles').each(($el, $index) => {
+      cy.wrap($el).children().eq(1).should('have.text', '')
+    })
+    
 
   })
 });
